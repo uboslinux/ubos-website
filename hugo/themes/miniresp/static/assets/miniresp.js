cbc86c817e03ef093f33073bfd4866902ef48e55
Johannes Ernst <Johannes.Ernst@gmail.com>
2019-12-26 19:08:46 -0800

    Fixing miniresp submodule

--- hugo/themes/miniresp/static/assets/miniresp.js
+++
@@ -1,8 +0,0 @@
-$(document).on("scroll",function(){
-    if($(document).scrollTop() > 100){
-        $("header").removeClass("header-large").addClass("header-small");
-    } else{
-        $("header").removeClass("header-small").addClass("header-large");
-    }
-});
-
